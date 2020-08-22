fb52de3401c693bad681538537c13b5e686366b8
skame <skame@nttv6.jp>
2020-08-23 00:19:58 +0900

    edit /etc/nsswitch.conf for ubuntu 20.04

--- nslcd-run.sh
+++ nslcd-run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ ! -e /etc/nslcd.conf ]; then
-  grep 'compat ldap' /etc/nsswitch.conf || sed -i 's/compat/compat ldap/' /etc/nsswitch.conf
+  sed -i 's/\(passwd:.*files\)/\1 ldap/' /etc/nsswitch.conf
+  sed -i 's/\(group:.*files\)/\1 ldap/' /etc/nsswitch.conf
   sigil -p -f /etc/nslcd.conf-template > /etc/nslcd.conf
   chmod o-r /etc/nslcd.conf
 fi 
